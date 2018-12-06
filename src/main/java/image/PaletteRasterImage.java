27aa285c1030ecf52ba15443096ed2e70ac3c78e
t17001743 <samantha.thiel@etu.univ-amu.fr>
2018-12-06 22:52:26 +0100

    Implémentation des constructeurs et des méthodes de PaletteRasterImage (Tâche 2)

--- src/main/java/image/PaletteRasterImage.java
+++ src/main/java/image/PaletteRasterImage.java
@@ -15,27 +15,47 @@ public class PaletteRasterImage implements Image {
     }
 
     public PaletteRasterImage(Color[][] pixels){
+        this.indexesOfColors = indexesOfColors;
         this.width = indexesOfColors.length;
         if(width == 0){
             throw new IllegalArgumentException("The matrix should not have a dimension equals to 0.");
         }
         this.height = indexesOfColors[0].length;
-        this.indexesOfColors = indexesOfColors;
     }
 
     public void createRepresentation(){
+        List<Color> palette = this.palette;
+        int[][] indexesOfColors = this.indexesOfColors;
     }
 
     public void setPixelColor(Color color, int x, int y){
+        Color pixelColor = palette.get(indexesOfColors[x][y]);
+        pixelColor = color;
+
+        for(int i = 0; i < indexesOfColors.length; i++){
+            for(int j = 0; j < indexesOfColors.length; j++){
+                if(palette.get(indexesOfColors[i][j]) != color){
+                    palette.add(color);
+                }
+            }
+        }
     }
 
     public Color getPixelColor(int x, int y){
+        return palette.get(indexesOfColors[x][y]);
     }
 
-    private void setPixelsColor(Color[][] pixels){
-    }
+    /*private void setPixelsColor(Color[][] pixels){
+        Color[][] matrix = new PaletteRasterImage(pixels);
+      }*/
 
     private void setPixelsColor(Color color){
+        for(int i = 0; i < indexesOfColors.length; i++){
+            for(int j = 0; j < indexesOfColors.length; j++){
+                Color pixelColor = getPixelColor(i, j);
+                pixelColor = color;
+            }
+        }
     }
 
     public int getWidth(){
